85ed595958c191003fb5bce7415d7c3af9aff15a
jakubkriz <jakub@izone.cz>
2016-11-06 21:45:18 +0100

    setup development npm script

--- src/counter.js
+++ src/counter.js
@@ -3,7 +3,7 @@ import { h2 } from '@motorcycle/dom'
 
 export default function counter ({ DOM }) {
   const sinks = {
-    DOM: periodic(1000, 1).scan((acc, i) => acc + i, 0).map(i => h2('' + i + ' seconds elapsed'))
+    DOM: periodic(1000, 1).scan((acc, i) => acc + i, 0).map(i => h2('' + i + ' seconds elapsed.'))
   }
 
   return sinks
